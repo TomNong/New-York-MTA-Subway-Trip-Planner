26dd29bd06423c144c3c1f5ed13b6e55e0d052e7
TomNong <TomNong@users.noreply.github.com>
2015-12-23 01:06:09 -0500

    Update configure.py

--- ble/configure.py
+++ ble/configure.py
@@ -1,5 +1,5 @@
 AWS_ACCESS_KEY = ""
 AWS_SECRET_ACCESS_KEY = ""
-#If you are using virginia server, you should be "us-east-1"
+#If you are using Virginia server, you should be "us-east-1"
 AWS_REGION = ""
 topicArn = ""
